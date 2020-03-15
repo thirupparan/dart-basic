daff36ea92815b742c8fbd1ddce06c9e2942ad1e
Thirupparan <thirupparan1994@gmail.com>
2020-03-15 23:07:38 +0530

    if and elase in dart

---
+++ if_else.dart
@@ -0,0 +1,21 @@
+
+6
+void main() {
+  printOddEven(6);
+}
+ 
+  void printOddEven(int value){
+    if(value % 2 == 0){
+      print('$value is even');
+    }else{
+      print('$value is odd');
+    }
+  }
+​
+​
+Console
+6 is even
+Documentation
+Privacy notice Send feedback
+no issues
+Based on Dart SDK 2.7.1
