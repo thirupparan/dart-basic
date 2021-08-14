317a397afdc974d99f34f4608941601c49c52cc2
Thirupparan <thirupparan1994@gmail.com>
2021-08-14 11:09:05 +0530

    number of week per year

--- date.dart
+++ date.dart
@@ -10,3 +10,10 @@ var dayOfWeek = 1;
         .subtract(Duration(days: date.weekday - dayOfWeek))
         .toIso8601String();
     print(lastMonday);
+
+//number of week pe year 
+  int numOfWeeks(int year) {
+    DateTime dec28 = DateTime(year, 12, 28);
+    int dayOfDec28 = int.parse(DateFormat("D").format(dec28));
+    return ((dayOfDec28 - dec28.weekday + 10) / 7).floor();
+  }
