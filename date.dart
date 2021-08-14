88e38f35a45f6f81d300581cf3a0894e2f421342
Thirupparan <thirupparan1994@gmail.com>
2021-08-14 11:06:40 +0530

    most recent monday find

--- date.dart
+++ date.dart
@@ -2,3 +2,11 @@ int weekNumber(DateTime date) {
   int dayOfYear = int.parse(DateFormat("D").format(date));
   return ((dayOfYear - date.weekday + 10) / 7).floor();
 }
+
+// Most recent monday find 
+var dayOfWeek = 1;
+    DateTime date = DateTime.now();
+    var lastMonday = date
+        .subtract(Duration(days: date.weekday - dayOfWeek))
+        .toIso8601String();
+    print(lastMonday);
