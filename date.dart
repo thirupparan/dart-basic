7e46f0e7ce39d47c7b4cf6fa2a8034c6ce376e09
Thirupparan <thirupparan1994@gmail.com>
2021-08-14 11:10:54 +0530

    week number find

--- date.dart
+++ date.dart
@@ -17,3 +17,14 @@ var dayOfWeek = 1;
     int dayOfDec28 = int.parse(DateFormat("D").format(dec28));
     return ((dayOfDec28 - dec28.weekday + 10) / 7).floor();
   }
+
+int weekNumber(DateTime date) {
+  int dayOfYear = int.parse(DateFormat("D").format(date));
+  int woy =  ((dayOfYear - date.weekday + 10) / 7).floor();
+  if (woy < 1) {
+    woy = numOfWeeks(date.year - 1);
+  } else if (woy > numOfWeeks(date.year)) {
+    woy = 1;
+  }
+  return woy;
+}
